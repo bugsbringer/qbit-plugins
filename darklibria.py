7bff6c6765da6331e7dfbbef23a0371e14133cbe
EugeneShaidulov <dastins193@gmail.com>
2021-03-07 22:24:27 +0200

    perf(dl): :rocket: rewrite plugin

--- darklibria.py
+++ darklibria.py
@@ -1,57 +1,107 @@
-#VERSION: 0.12
+#VERSION: 0.13
 #AUTHORS: Bugsbringer (dastins193@gmail.com)
 
-import concurrent.futures
+
+SITE_URL = 'https://darklibria.it/'
+
+
 import logging
 import os
-import re
+from concurrent.futures import ThreadPoolExecutor
+from datetime import datetime
 from html.parser import HTMLParser
-from json import dumps
+from math import ceil
+from re import compile as re_compile
+from time import mktime
 from urllib import parse
 
 from helpers import retrieve_url
 from novaprinter import prettyPrinter
 
-STORAGE = os.path.abspath(os.path.dirname(__file__))
-
 LOG_FORMAT = '[%(asctime)s] %(levelname)s:%(name)s:%(funcName)s - %(message)s'
 LOG_DT_FORMAT = '%d-%b-%y %H:%M:%S'
 
-if __name__ == '__main__':
-    logging.basicConfig(level='DEBUG', format=LOG_FORMAT, datefmt=LOG_DT_FORMAT)
-else:
-    logging.basicConfig(level='WARNING', filename=os.path.join(STORAGE, 'darklibria.log'), format=LOG_FORMAT, datefmt=LOG_DT_FORMAT)
-
-logger = logging.getLogger('darklibria')
-logger.setLevel(logging.WARNING)
-
 
 class darklibria:
-    url = 'https://dark-libria.it/'
+    url = SITE_URL
     name = 'dark-libria'
     supported_categories = {'all': '0'}
-    
+
     units_dict = {"Тб": "TB", "Гб": "GB", "Мб": "MB", "Кб": "KB", "б": "B"}
-    page_search_url_pattern = 'https://dark-libria.it/search?page={page}&find={what}'
+    page_search_url_pattern = SITE_URL + 'search?page={page}&find={what}'
+    dt_regex = re_compile('\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}')
 
     def __init__(self, output=True):
         self.output = output
 
     def search(self, what, cat='all'):
-        logger.info(what)
-
         self.torrents_count = 0
         what = parse.quote(parse.unquote(what))
-
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            for page in range(2, self.handle_page(what, 1) + 1):
+        logger.info(parse.unquote(what))
+        self.set_search_data(self.handle_page(what, 1))
+        with ThreadPoolExecutor() as executor:
+            for page in range(2, self.pages_count + 1):
                 executor.submit(self.handle_page, what, page)
-
         logger.info('%s torrents', self.torrents_count)
 
     def handle_page(self, what, page):
+        url = self.page_search_url_pattern.format(page=page, what=what)
+        data = self.request_get(url)
+        if not data:
+            return
+        parser = Parser(data)
+        serials = parser.find_all('tbody', {'style': 'vertical-align: center'})
+        with ThreadPoolExecutor() as executor:
+            for serial in serials:
+                executor.submit(self.handle_serial, serial.a['href'])
+        return parser
+
+    def handle_serial(self, url):
+        data = self.request_get(url)
+        if not data:
+            return
+        parser = Parser(data)
+        name = parser.find(attrs={'id': 'russian_name'}).text
+        for torrent_row in parser.find_all('tr', {'class': 'torrent'}):
+            self.handle_torrent_row(torrent_row, name, url)
+
+    def handle_torrent_row(self, torrent_row, name, url):
+        type, quality, size_data, date_time, download, seeds, leech, *_ = torrent_row.children
+        self.pretty_printer({
+            'link': self.get_link(download),
+            'name': self.get_name(name, quality, type, date_time),
+            'size': self.get_size(size_data),
+            'seeds': int(seeds.text),
+            'leech': int(leech.text),
+            'engine_url': self.url,
+            'desc_link': url
+        })
+        self.torrents_count += 1
+
+    def get_link(self, download):
+        return download.find(attrs={'title': 'Magnet-ссылка'})['href'] \
+            or download.find(attrs={'title': 'Скачать торрент'})['href']
+            
+    def get_name(self, name, quality, type, date_time):
+        return '[{}] {} [{}] {}'.format(
+            self.get_date(date_time),
+            name,
+            type.text,
+            quality.text
+        )
+
+    def get_date(self, date_time):
+        utc_dt_string = self.dt_regex.search(date_time.text).group()
+        utc = datetime.strptime(utc_dt_string, '%Y-%m-%d %H:%M:%S')
+        return str(utc2local(utc))
+
+    def get_size(self, size_data):
+        size, unit = size_data.text.split()
+        return size + ' ' + self.units_dict[unit]
+
+    def request_get(self, url):
         try:
-            parser = Parser(retrieve_url(self.page_search_url_pattern.format(page=page, what=what)))
+            return retrieve_url(url)
         except Exception as exp:
             logger.error(exp)
             self.pretty_printer({
@@ -64,81 +114,24 @@ class darklibria:
                 'desc_link': self.url
             })
 
-            return 0
-
-        if page == 1:
-            pages_div = parser.find('div', {'class': 'bg-dark d-sm-block d-none'})
-            pages_count = re.search(r'(?<=page=)\d+', pages_div.find_all('li')[-1].a['href'])[0]
-
-            logger.info('%s pages', pages_count)
-
-        torrents_table = parser.find('div', {'id': 'torrents_table'}).tbody
-        torrents = torrents_table.find_all('tr', {'class': 'torrent'})
-
-        logger.info('[page %s] %s animes', page, len(torrents))
-
-        for torrent in torrents:
-            name = torrent.a.span.text
-            desc_link = torrent.a['href']
-            
-            qualities = []
-            quality_cell = torrent.find('td', {'class': 'torrent d-none d-lg-table-cell'})
-
-            for quality in quality_cell.find_all('ul', {'class': 'torrent'}):
-                qualities.append(parse.unquote(quality.text))
-
-            other_cells = torrent.find_all('td', {'class': 'torrent text-center'})
-            
-            episodes = []
-            for episode in other_cells[0].find_all('ul', {'class': 'torrent'}):
-                episodes.append(episode.text)
-
-            sizes = []
-            for size in other_cells[1].find_all('ul', {'class': 'torrent'}):
-                sizes.append(parse.unquote(size.text))
-
-            links = []
-            for link in other_cells[3].find_all('ul', {'class': 'torrent'}):
-                links.append(link.a['href'])
-            
-            seeders = []
-            seeds_cell = torrent.find('td', {'class': 'torrent text-success text-center d-none d-lg-table-cell'})
-            for seed in seeds_cell.find_all('ul', {'class': 'torrent'}):
-                seeders.append(seed.text)
-
-            leechers = []
-            leechs_cell = torrent.find('td', {'class': 'torrent text-danger text-center d-none d-lg-table-cell'})
-            for leech in leechs_cell.find_all('ul', {'class': 'torrent'}):
-                leechers.append(leech.text)
-
-            data = zip(qualities, episodes, links, sizes, seeders, leechers)
-
-            for qual, ep, link, size, seeds, leechs in data:
-                size, unit = size.split()
-
-                self.pretty_printer({
-                    'link': self.url + link,
-                    'name': ' '.join((name, qual, ep)),
-                    'size': size + ' ' + self.units_dict[unit],
-                    'seeds': int(seeds),
-                    'leech': int(leechs),
-                    'engine_url': self.url,
-                    'desc_link': self.url + desc_link
-                })
-            
-        if page == 1:
-            return int(pages_count)
-
     def pretty_printer(self, dictionary):
-        data = dumps(dictionary, sort_keys=True, indent=4)
-        if dictionary['link'] == 'Error':
-            logger.error(data)
-        else:
-            logger.debug(data)
-            self.torrents_count += 1
-
+        logger.debug(str(dictionary))
         if self.output:
             prettyPrinter(dictionary)
+
+    def set_search_data(self, parser):
+        results = parser.find('span', {'class': 'text text-light mt-0'})
+        if results:
+            parts = results.text.split()
+            items_count = int(parts[4])
+            items_on_page = int(parts[2].split('-')[1])
+            self.pages_count = ceil(items_count / items_on_page)
+
+            logger.info('%s animes', items_count)
+        else:
+            self.pages_count = 0
+
+        logger.info('%s pages', self.pages_count)
 
 
 class Tag:
@@ -147,7 +140,7 @@ class Tag:
         self.is_self_closing = is_self_closing
         self._attrs = tuple(attrs)
         self._content = tuple()
-        
+
     @property
     def attrs(self):
         """returns dict of Tag's attrs"""
@@ -162,26 +155,26 @@ class Tag:
         if isinstance(obj, (Tag, str)):
             self._content += (obj,)
         else:
-            raise TypeError('Argument must be str or %s, not %s' % (self.__class__, obj.__class__))
+            raise TypeError('Argument must be str or %s, not %s' %
+                            (self.__class__, obj.__class__))
 
     def find(self, tag=None, attrs=None):
         """returns Tag or None"""
-
         return next(self._find_all(tag, attrs), None)
 
     def find_all(self, tag=None, attrs=None):
         """returns list"""
-
         return list(self._find_all(tag, attrs))
-        
+
     def _find_all(self, tag_type=None, attrs=None):
         """returns generator"""
-
         if not (isinstance(tag_type, (str, Tag)) or tag_type is None):
-            raise TypeError('tag_type argument must be str or Tag, not %s' % (tag_type.__class__))
+            raise TypeError(
+                'tag_type argument must be str or Tag, not %s' % (tag_type.__class__))
 
         if not (isinstance(attrs, dict) or attrs is None):
-            raise TypeError('attrs argument must be dict, not %s' % (self.__class__))
+            raise TypeError('attrs argument must be dict, not %s' %
+                            (self.__class__))
 
         # get tags-descendants generator
         results = self.descendants
@@ -189,7 +182,8 @@ class Tag:
         # filter by Tag.type
         if tag_type:
             if isinstance(tag_type, Tag):
-                tag_type, attrs = tag_type.type, (attrs if attrs else tag_type.attrs)
+                tag_type, attrs = tag_type.type, (
+                    attrs if attrs else tag_type.attrs)
 
             results = filter(lambda t: t.type == tag_type, results)
 
@@ -203,22 +197,20 @@ class Tag:
                     if attrs[key] not in tag.attrs.get(key, ()):
                         return False
                 return True
-            
+
             # filter by attrs
             results = filter(filter_func, results)
-        
+
         yield from results
 
     @property
     def children(self):
         """returns generator of tags-children"""
-
         return (obj for obj in self._content if isinstance(obj, Tag))
 
     @property
     def descendants(self):
         """returns generator of tags-descendants"""
-
         for child_tag in self.children:
             yield child_tag
             yield from child_tag.descendants
@@ -231,23 +223,25 @@ class Tag:
             return self.find(tag=attr)
 
     def __repr__(self):
-        attrs = ' '.join(str(k) if v is None else '{}="{}"'.format(k, v) for k, v in self._attrs)
+        attrs = ' '.join(str(k) if v is None else '{}="{}"'.format(k, v)
+                         for k, v in self._attrs)
         starttag = ' '.join((self.type, attrs)) if attrs else self.type
 
         if self.is_self_closing:
             return '<{starttag}>\n'.format(starttag=starttag)
         else:
-            nested = '\n' * bool(next(self.children, None)) + ''.join(map(str, self._content))
+            nested = '\n' * bool(next(self.children, None)) + \
+                ''.join(map(str, self._content))
             return '<{}>{}</{}>\n'.format(starttag, nested, self.type)
 
-            
+
 class Parser(HTMLParser):
     def __init__(self, html_code, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self._root = Tag('_root')
         self._path = [self._root]
-        
+
         self.feed(''.join(map(str.strip, html_code.splitlines())))
         self.handle_endtag(self._root.type)
         self.close()
@@ -301,18 +295,23 @@ class Parser(HTMLParser):
         return ''.join(str(c) for c in self._root._content)
 
 
-if __name__ == '__main__':
-    import sys
-    
-    if 1 < len(sys.argv) < 4:
+def utc2local(utc):
+    epoch = mktime(utc.timetuple())
+    offset = datetime.fromtimestamp(epoch) - datetime.utcfromtimestamp(epoch)
+    return utc + offset
 
-        if len(sys.argv) == 3:
-            if sys.argv[1] == '-d':
-                logger.setLevel(logging.DEBUG)
-            
-            else:
-                exit('%s [-d] "search_query"' % (__file__))
-        else:
-            logger.setLevel(logging.INFO)
-        
-        darklibria(output=False).search(sys.argv[-1])
+
+is_main = __name__ == '__main__'
+STORAGE = os.path.abspath(os.path.dirname(__file__))
+log_config = {
+    'level': logging.INFO if is_main else logging.WARNING,
+    'filename': None if is_main else os.path.join(STORAGE, 'darklibria.log'),
+    'format': LOG_FORMAT,
+    'datefmt': LOG_DT_FORMAT
+}
+logging.basicConfig(**log_config)
+logger = logging.getLogger('darklibria')
+
+if is_main:
+    import sys
+    darklibria(output=False).search(sys.argv[-1])
